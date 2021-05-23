f1bb1c17d79649f06717abee2fc3100b252ad196
bakerv <80018640+bakerv@users.noreply.github.com>
2021-05-22 21:24:29 -0500

    troubleshooting converting nonunique index values into multiple dictionary keys

--- app.py
+++ app.py
@@ -3,8 +3,22 @@ import pandas as pd
 from sqlalchemy import create_engine,inspect,func
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
+import pandas as pd
 
 app = Flask(__name__)
+
+# Import data from sqlite using SQLalhchemy. Use refelct to automate schema and class creation
+engine = create_engine("sqlite:///Data/hawaii.sqlite")
+Base = automap_base()
+Base.prepare(engine,reflect = True)
+
+# initialize the query tool
+session = Session(engine)
+
+# Create classes for each data table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
 
 @app.route("/")
 def home():
@@ -22,26 +36,33 @@ def home():
 def precipitation():
     """Return all precipitaion data in a list of dictionaries. 
     Uses the date as the key, and precipiation amount as the value"""
-    return('Precipiation Page')
 
-@app.route("/api/v1.0/stations")
-def stations():
+    rainfall = pd.read_sql("""
+SELECT date AS Date, prcp As Precipitation
+FROM measurement 
+WHERE Date > DATE('2016-08-23')
+ORDER BY Date ASC;""",con = engine)
+  
+    return(rainfall.T.to_json(orient='records'))
+
+#@app.route("/api/v1.0/stations")
+#def stations():
     return(
         f""
         f""
         f""
     )
 
-@app.route("/api/v1.0/tobs")
-def stations():
+#@app.route("/api/v1.0/tobs")
+#def stations():
     return(
         f""
         f""
         f""
     )
 
-@app.route("/api/v1.0/<start>")
-def stations():
+#@app.route("/api/v1.0/<start>")
+#def stations():
     return(
         f""
         f""
