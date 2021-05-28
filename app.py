9da96409761d3c4dc35f20819027484a7397b22e
bakerv <80018640+bakerv@users.noreply.github.com>
2021-05-27 22:29:43 -0500

    a little bit of polishing

--- app.py
+++ app.py
@@ -40,10 +40,11 @@ def precipitation():
     """Return all precipitaion data in a list of dictionaries. 
     Uses the date as the key, and precipiation amount as the value"""
     rainfall = pd.read_sql("""
-SELECT date AS Date, prcp As Precipitation
-FROM measurement 
-WHERE Date > DATE('2016-08-23')
-ORDER BY Date ASC;""",con = engine).dropna()
+    SELECT date AS Date, prcp As Precipitation
+    FROM measurement 
+    WHERE Date > DATE('2016-08-23')
+    ORDER BY Date ASC;""",con = engine).dropna()
+
     rainfalldict = {}
     for rows in rainfall.itertuples():
         rainfalldict[rows[0]] = ({rows[1]:rows[2]})
@@ -60,7 +61,7 @@ def stations():
 @app.route("/api/v1.0/tobs")
 def tobs():
     with engine.connect() as data:
-         mostactive = list(np.ravel(data.execute("""
+        mostactive = list(np.ravel(data.execute("""
         SELECT station
         FROM measurement
         GROUP BY station
@@ -72,7 +73,7 @@ def tobs():
         previous_year = data.execute("""
         SELECT date AS Date, tobs AS Temperature
         FROM measurement
-        WHERE date > DATE('2016-08-23') and station = :ma"""
+        WHERE date > DATE('2016-08-23') and station = :ma;"""
         , {'ma':mostactive[0]}).all()    
     return(jsonify([dict(row) for row in previous_year]))
 
@@ -83,9 +84,10 @@ def start(start):
         SELECT MIN(tobs) AS Temperature_Minimum,
         MAX(tobs) AS Temperature_Maximum,
         ROUND(AVG(tobs),0) AS Temperature_Average,
-        MIN(date) AS Date_Beginning, MAX(date) AS Date_Ending
+        MIN(date) AS Date_Beginning,
+        MAX(date) AS Date_Ending
         FROM measurement
-        WHERE date > :start"""
+        WHERE date > :start;"""
         , {'start':start}).all()
 
 
@@ -99,10 +101,12 @@ def start_end(start,end):
         SELECT MIN(tobs) AS Temperature_Minimum,
         MAX(tobs) AS Temperature_Maximum,
         ROUND(AVG(tobs),0) AS Temperature_Average,
-        MIN(date) AS Date_Beginning, MAX(date) AS Date_Ending
+        MIN(date) AS Date_Beginning,
+        MAX(date) AS Date_Ending
         FROM measurement
-        WHERE date >= :start AND date <= :end """
+        WHERE date >= :start AND date <= :end; """
         , {'start':start,'end':end}).all()
+
     return(jsonify([dict(row) for row in tobs_stats_se]))
 if __name__ == "__main__":
     app.run(debug=True)
