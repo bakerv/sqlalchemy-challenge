eff5cec728e56d229d54214bb7466eb8cfea82f1
bakerv <80018640+bakerv@users.noreply.github.com>
2021-05-27 21:43:27 -0500

    debugging sqlalchemy queries for routes

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from sqlalchemy import create_engine,inspect,func
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 import pandas as pd
+import numpy as np
 
 app = Flask(__name__)
 
@@ -20,19 +21,12 @@ Measurement = Base.classes.measurement
 Station = Base.classes.station
 
 # Create rainfall dictionary
-rainfall = pd.read_sql("""
-SELECT date AS Date, prcp As Precipitation
-FROM measurement 
-WHERE Date > DATE('2016-08-23')
-ORDER BY Date ASC;""",con = engine).dropna()
-rainfalldict = {}
-for rows in rainfall.itertuples():
-    rainfalldict[rows[0]] = ({rows[1]:rows[2]})
+
 
 @app.route("/")
 def home():
     return(
-        f"This API will return weather Hawaii Weather Data.<br/>"
+        f"This API will return Hawaii Weather Data.<br/>"
         f"Available Routes: <br/>"
         f"/api/v1.0/precipitation <br/>"
         f"/api/v1.0/stations <br/>"
@@ -45,16 +39,42 @@ def home():
 def precipitation():
     """Return all precipitaion data in a list of dictionaries. 
     Uses the date as the key, and precipiation amount as the value"""
-    return(jsonify(rainfalldict))
+    rainfall = pd.read_sql("""
+SELECT date AS Date, prcp As Precipitation
+FROM measurement 
+WHERE Date > DATE('2016-08-23')
+ORDER BY Date ASC;""",con = engine).dropna()
+    rainfalldict = {}
+    for rows in rainfall.itertuples():
+        rainfalldict[rows[0]] = ({rows[1]:rows[2]})
+    return(jsonify([rainfalldict]))
 
 
 
 @app.route("/api/v1.0/stations")
 def stations():
-    return(session.query(Station.station))
+    station_list = session.query(Station.station).all()
+    all_names = list(np.ravel(station_list))
+    return(jsonify(all_names))
 
-#@app.route("/api/v1.0/tobs")
-#def stations():
+@app.route("/api/v1.0/tobs")
+def tobs():
+    with engine.connect() as data:
+         mostactive = list(np.ravel(data.execute("""
+        SELECT station
+        FROM measurement
+        GROUP BY station
+        ORDER BY count(prcp) DESC
+        LIMIT 1;
+        """).all()))
+
+    with engine.connect() as data:
+        previous_year = data.execute("""
+        SELECT date, prcp
+        FROM measurement
+        WHERE date > DATE('2016-08-23') and station = :ma"""
+        , {'ma':mostactive[0]}).all()    
+    return(jsonify([dict(row) for row in previous_year]))
     # return(
        # f""
       #  f""
@@ -76,5 +96,5 @@ def stations():
      #   f""
       #  f""
     #)
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
